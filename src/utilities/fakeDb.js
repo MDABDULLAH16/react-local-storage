809ddb650173d736e928ac26cfc043e594efc623
MD ABDULLAH <mdabdullah161036@gmail.com>
2024-01-04 19:03:21 +0600

    add object to localStorage

--- src/utilities/fakeDb.js
+++ src/utilities/fakeDb.js
@@ -1,10 +1,21 @@
 const fakeDb = (id) => {
-  const quantity = localStorage.getItem(id);
+  let shoppingCart = {};
+  //get from local storage
+  const getStored = localStorage.getItem("shopping-cart");
+  if (getStored) {
+    shoppingCart = JSON.parse(getStored);
+  } else {
+    shoppingCart = {};
+  }
+
+  //set item to local storage
+  const quantity = shoppingCart[id];
   if (quantity) {
     const newQuantity = quantity + 1;
-    localStorage.setItem(id, newQuantity);
+    shoppingCart[id] = newQuantity;
   } else {
-    localStorage.setItem(id, 1);
+    shoppingCart[id] = 1;
   }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 export { fakeDb };
